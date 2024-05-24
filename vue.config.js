b869b64e68c956db7958e0bda12ca942375698fb
LinChengLi <1019034271@qq.com>
2024-05-24 20:31:42 +0800

    feat: 关闭sourceMap

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false,
   devServer: {
     host: '0.0.0.0',
     port: 4000, // 端口号
